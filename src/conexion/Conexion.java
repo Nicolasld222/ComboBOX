4258508558969a13cd7541a7eba954f0ab0177f4
Nicolasld222 <nicolaslondia@gmail.com>
2025-06-11 18:51:51 -0500

    Agregue la carpetas properties

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -6,37 +6,36 @@ import java.sql.SQLException;
 
 
 public class Conexion {
-    private String nombreBd="persona";
-    private String usuario="root";
-    private String password="";
-    private String url="jdbc:mysql://localhost:3306/"+nombreBd+"?useUnicode=true&use"
-            + "JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&"
-            + "serverTimezone=UTC";
-    static Connection conn=null;
-    //constructor de la clase
-    public Conexion(){
+    private String nombreBd = Propiedades.get("nombre_bd");
+    private String usuario = Propiedades.get("usuario_bd");
+    private String password = Propiedades.get("clave_bd");
+    private String url = Propiedades.get("url_bd");
+    private String driver = Propiedades.get("driver_bd");
+
+    static Connection conn = null;
+
+    public Conexion() {
         try {
-            //obtener el driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            //obtener la conexion
-            conn=DriverManager.getConnection(url,usuario,password);
-            if (conn!=null) {
-                System.out.println("Conexion Exitosa a la BD: "+nombreBd);
-            }else{
-                System.out.println("******************NO SE PUDO CONECTAR "+nombreBd);
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, usuario, password);
+            if (conn != null) {
+                System.out.println("Conexión Exitosa a la BD: " + nombreBd);
+            } else {
+                System.out.println("****************** NO SE PUDO CONECTAR A " + nombreBd);
             }
-        }
-        catch (ClassNotFoundException e) {
-            System.out.println("ocurre una ClassNotFoundException : "+e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Ocurre una ClassNotFoundException: " + e.getMessage());
         } catch (SQLException e) {
-            System.out.println("ocurre una SQLException: "+e.getMessage());
-            System.out.println("Verifique que Mysql esté encendido...");
+            System.out.println("Ocurre una SQLException: " + e.getMessage());
+            System.out.println("Verifique que MySQL esté encendido...");
         }
     }
-    public static Connection getConnection(){
+
+    public static Connection getConnection() {
         return conn;
     }
-    public void desconectar(){
-        conn=null;
+
+    public void desconectar() {
+        conn = null;
     }
 }
